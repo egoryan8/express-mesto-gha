e9405c768037c22bc242e2e1349f0d25b711697d
egoryan8 <fogel_92@bk.ru>
2022-11-20 00:01:55 +0700

    fix: 401 error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,22 @@
-const jwt = require('jsonwebtoken');
-const AuthorizedError = require('../utils/errorClasses/AuthorizedError');
+const jwt = require("jsonwebtoken");
+const AuthorizedError = require("../utils/errorClasses/AuthorizedError");
 
-const { JWT_SECRET } = require('../utils/constants');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   let payload;
   try {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(" ")[1];
     if (!token) {
-      return next(new AuthorizedError('Нужно авторизоваться!'));
+      return next(new AuthorizedError("Нужно авторизоваться!"));
     }
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : "dev-secret"
+    );
   } catch (e) {
-    return next(new AuthorizedError('Нужно авторизоваться!'));
+    return next(new AuthorizedError("Нужно авторизоваться!"));
   }
   req.user = payload;
   next();
