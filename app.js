3095f6191130f8ee530c92183322a3ba2a817ebf
Fogel Egor <fogel_92@bk.ru>
2022-11-19 14:06:04 +0700

    fix: cors

--- app.js
+++ app.js
@@ -16,28 +16,24 @@ const cors = require('./middlewares/cors');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors);
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mestodb');
-
 app.use(requestLogger);
+app.use(cors);
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
 app.post('/signin', loginValidation, login);
 app.post('/signup', registerValidation, createUser);
 
 app.use(auth);
-
-app.use('/users', userRouter);
 app.use('/cards', cardRouter);
+app.use('/users', userRouter);
 app.use('*', () => {
-  throw new NotFoundError('Ничего не найдено. Проверьте URL и метод запроса');
+  throw new NotFoundError('Запрашиваемый адрес не найден. Проверьте URL и метод запроса');
 });
-
 app.use(errorLogger);
 app.use(errors());
 app.use(error);
