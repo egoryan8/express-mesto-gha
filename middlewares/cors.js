649e8b23d562ae430c5f21fcbb6a754bab7d341b
Fogel Egor <fogel_92@bk.ru>
2022-11-21 14:57:51 +0700

    fix: errors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,8 @@
 const allowedCors = [
-  'https://api.dacorm.nomoredomains.icu',
+  'https://mesto.fogel.nomoredomains.icu',
   'http://mesto.fogel.nomoredomains.icu',
-  'https://localhost:3001',
-  'http://localhost:3001',
+  'https://localhost:3000',
+  'http://localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
